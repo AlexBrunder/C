008605333bfadf10abe114a50fdcab12e619f9fa
Alexbrunder <986392841@qq.com>
2023-04-16 14:05:11 +0800

    修复了一些二叉树的使用bug

--- 二叉树的操作/二叉树的操作/二叉树的操作/二叉树的操作.c
+++ 二叉树的操作/二叉树的操作/二叉树的操作/二叉树的操作.c
@@ -28,28 +28,24 @@ Data* CreatData(char a)
 //返回值：新树的地址
 Data* CreatePreTree(Data* T,int a)
 {
-	Data* p= (Data*)malloc(sizeof(Data));
-	if (a <6)
-	{ 
-		char ch;
-		scanf_s("%c", &ch);
+
+	char ch;
+	scanf_s("%c", &ch);	
+	if (ch == '#')
+	{
+		T = NULL;
+	}
+	else
+	{		
+		T = CreatData(ch);	
 		a++;
-		if (ch == '#')
-		{
-			T = NULL;
-		}
-		else
-		{		
-			T = CreatData(ch);	
-			a++;
-			T->Ltree = CreatePreTree(T->Ltree,a);
-			T->Rtree = CreatePreTree(T->Rtree,a);
-		}
+		T->Ltree = CreatePreTree(T->Ltree,a);
+		T->Rtree = CreatePreTree(T->Rtree,a);
 	}
 	return T;
 }
 
-//先序遍历的算法（输出节点的值）
+//先序遍历的算法（输出节点的值）DLR
 //参数：树的地址
 //返回值：0：为空值 1：成功
 int PreOrderTraverse(Data* T)
@@ -66,11 +62,91 @@ int PreOrderTraverse(Data* T)
 	}
 }
 
+//中序遍历的算法（输出节点的值）LDR
+//参数：树的地址
+//返回值：0：为空值 1：成功
+int MidOrderTraverse(Data* T)
+{
+	if (T == NULL)
+	{
+		return 0;
+	}
+	else
+	{
+		MidOrderTraverse(T->Ltree);
+		printf("%c", T->data);
+		MidOrderTraverse(T->Rtree);
+		
+	}	
+}
+
+
+//后序遍历的算法（输出节点的值）LRD
+//参数：树的地址
+//返回值：0：为空值 1：成功
+int BackOrderTraverse(Data* T)
+{
+	if (T == NULL)
+	{
+		return 0;
+	}
+	else
+	{
+		BackOrderTraverse(T->Ltree);
+		BackOrderTraverse(T->Rtree);
+		printf("%c\n", T->data);
+	}
+}
+
+//计算二叉树的深度算法
+//参数：树的地址
+//返回值：树的深度int
+int Depth(Data* T)
+{
+	int m=0, n=0;
+	if(T==NULL)
+	{
+		return 0;//深度为0
+	}
+	else
+	{
+		m = Depth(T->Ltree);
+		n = Depth(T->Rtree);
+		if (m > n)
+		{
+			return m+1;
+		}
+		else
+		{
+			return n+1;
+		}
+	}
+}
+
+//计算叶子节点的个数
+//参数：树的地址
+//返回值：个数int
+void LeadCount(Data* T,int*i)
+{
+	if (T == NULL)
+	{
+		return 0;
+	}
+	if(T->Ltree == NULL && T->Rtree == NULL)
+	{
+		(*i)++;
+	}	
+    LeadCount(T->Ltree,i);
+	LeadCount(T->Rtree, i);
+}
+
+
 void main()
 {
-	Data* FirstTree =(Data*)malloc(sizeof(Data));
-	FirstTree = CreatData('a');
-	FirstTree= CreatePreTree(FirstTree,0);
+	Data* FirstTree = (Data*)malloc(sizeof(Data));
+	FirstTree = CreatePreTree(FirstTree, 0);
 	printf("\n");
-	int i = PreOrderTraverse(FirstTree);
+	int i = 0;
+	int* p = &i;
+	i = Depth(FirstTree, i);
 }
